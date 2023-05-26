f73fe21a7c8bda71c774f53e394816afa305e780
Esteban Sayago <sayagoo823@gmail.com>
2023-05-26 13:35:16 -0300

    actualizar contexto

--- src/contexto/IdiomaProvider.jsx
+++ src/contexto/IdiomaProvider.jsx
@@ -1,14 +1,20 @@
 import { idiomaContext } from './idioma'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import { getListOfGeners } from '../servicios/getListOfGeners'
 
 export function IdiomaProvider({ children }) {
   const [idioma, setIdioma] = useState('es-MX')
+  const [generos, setGeneros] = useState([])
   const updateLanguage = (evento) => {
     setIdioma(evento.target.value)
   }
 
+  useEffect(() => {
+    getListOfGeners({ lang: idioma }).then((res) => setGeneros(res))
+  }, [idioma])
+
   return (
-    <idiomaContext.Provider value={{ idioma, updateLanguage }}>
+    <idiomaContext.Provider value={{ idioma, updateLanguage, generos }}>
       {children}
     </idiomaContext.Provider>
   )
