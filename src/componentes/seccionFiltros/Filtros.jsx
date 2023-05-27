5d5766cb61bd0050c49401dbb5c61e9c03e11372
Esteban Sayago <sayagoo823@gmail.com>
2023-05-27 08:23:35 -0300

    ajustar estilos

--- src/componentes/seccionFiltros/Filtros.jsx
+++ src/componentes/seccionFiltros/Filtros.jsx
@@ -1,11 +1,11 @@
-import { ListaDeCategorias } from './../listaCategorias/ListaDeCategorias'
-import { ListaTendencias } from './../listaTendencias/ListaTendencias'
-import { ListaDeGeneros } from './../listaDeGeneros/ListaDeGeneros'
+import { ListaDeCategorias } from './listaCategorias/ListaDeCategorias'
+import { ListaTendencias } from './listaTendencias/ListaTendencias'
+import { ListaDeGeneros } from './listaDeGeneros/ListaDeGeneros'
 import { Buscar } from './../buscarPeliculas/Buscar'
 
 export function Filtros() {
   return (
-    <div className='filtros'>
+    <div className='bg-base-300 rounded-md p-3 w-[25%] border-r-2'>
       <Buscar />
       <ListaDeCategorias />
       <ListaTendencias />
