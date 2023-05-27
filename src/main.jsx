645c8b99023a33064cbe6379776650eb99af3c23
Esteban Sayago <sayagoo823@gmail.com>
2023-05-27 12:17:40 -0300

    agregar paginacion para resultados de busqueda

--- src/main.jsx
+++ src/main.jsx
@@ -36,7 +36,7 @@ const router = createBrowserRouter([
         element: <PeliculaDetalles />,
       },
       {
-        path: '/busqueda',
+        path: '/busqueda/page?/:page?',
         element: <ResultadoBusqueda />,
       },
     ],
