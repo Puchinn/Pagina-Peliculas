332b769be2fbba8485f669bd4c5931940bf0c7f1
Esteban Sayago <sayagoo823@gmail.com>
2023-05-26 23:55:08 -0300

    actualizar estilos

--- src/componentes/buscarPeliculas/Buscar.jsx
+++ src/componentes/buscarPeliculas/Buscar.jsx
@@ -2,15 +2,15 @@ import { Form } from 'react-router-dom'
 
 export function Buscar() {
   return (
-    <Form action='/busqueda'>
+    <Form action='/busqueda' className='flex flex-col gap-y-3 my-2'>
       <input
-        className=''
+        className='input text-lg'
         type='text'
         name='query'
         placeholder='Buscar pelicula'
         required
       />
-      <button className=''>Buscar</button>
+      <button className='btn'>Buscar</button>
     </Form>
   )
 }
