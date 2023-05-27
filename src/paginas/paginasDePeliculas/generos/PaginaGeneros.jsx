4a13870cbd217ca722bbac3fe291a5288f922fcd
Esteban Sayago <sayagoo823@gmail.com>
2023-05-27 12:18:37 -0300

    utilizar componente abstraccion de paginas

--- src/paginas/paginasDePeliculas/generos/PaginaGeneros.jsx
+++ src/paginas/paginasDePeliculas/generos/PaginaGeneros.jsx
@@ -1,20 +1,14 @@
-import { Filtros } from '../../../componentes/seccionFiltros/Filtros'
 import { usePaginaGeneros } from './usePaginaGeneros'
-import { ListaDePeliculas } from '../../../componentes/listaDePeliculas/ListaDePeliculas'
-import { Paginacion } from './../../../componentes/paginacion/Paginacion'
+import { StyledPage } from '../styledPage/StyledPage'
 
 export function PaginaGeneros() {
   const { titulo, peliculas, paginas, url } = usePaginaGeneros()
   return (
-    <section className='peliculas'>
-      <h1 className='titulo'>Peliculas - {titulo} </h1>
-      <div className='contenedor'>
-        <Filtros />
-        <div className='peliculas_lista'>
-          <ListaDePeliculas peliculas={peliculas} />
-        </div>
-      </div>
-      <Paginacion paginas={paginas} url={url} />
-    </section>
+    <StyledPage
+      titulo={titulo}
+      peliculas={peliculas}
+      paginas={paginas}
+      url={url}
+    />
   )
 }
--- src/paginas/paginasDePeliculas/tendencias/PaginaTendencias.jsx
+++ src/paginas/paginasDePeliculas/tendencias/PaginaTendencias.jsx
@@ -1,21 +1,14 @@
-import { Filtros } from '../../../componentes/seccionFiltros/Filtros'
 import { useTendencias } from './useTendencias'
-import { ListaDePeliculas } from '../../../componentes/listaDePeliculas/ListaDePeliculas'
-import { Paginacion } from '../../../componentes/paginacion/Paginacion'
+import { StyledPage } from './../styledPage/StyledPage'
 
 export function PaginaTendencias() {
   const { titulo, peliculas, paginas, url } = useTendencias()
-
   return (
-    <section className='peliculas'>
-      <h1 className='titulo'>Peliculas - {titulo} </h1>
-      <div className='contenedor'>
-        <Filtros />
-        <div className='peliculas_lista'>
-          <ListaDePeliculas peliculas={peliculas} />
-        </div>
-      </div>
-      <Paginacion paginas={paginas} url={url} />
-    </section>
+    <StyledPage
+      titulo={titulo}
+      peliculas={peliculas}
+      paginas={paginas}
+      url={url}
+    />
   )
 }
