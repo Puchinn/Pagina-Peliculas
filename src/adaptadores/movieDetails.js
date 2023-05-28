3e2c2389198569327376879adcb9c071d8e7e4ed
Esteban Sayago <sayagoo823@gmail.com>
2023-05-28 14:36:44 -0300

    actualizar informacion entrante

--- src/adaptadores/movieDetails.js
+++ src/adaptadores/movieDetails.js
@@ -2,8 +2,13 @@ export function movideDetails({ obj }) {
   return {
     id: obj.id,
     titulo: obj.title,
-    posterImg: 'https://image.tmdb.org/t/p/w400' + obj.poster_path,
+    posterImg: 'https://image.tmdb.org/t/p/w500' + obj.poster_path,
     fondoImg: 'https://image.tmdb.org/t/p/original' + obj.backdrop_path,
-    descripcion: obj.overview
+    descripcion: obj.overview,
+    generos: obj.genres,
+    subTitulo: obj.tagline,
+    lanzamiento: obj.release_date,
+    popularidad: obj.popularity,
+    calificacion: obj.vote_average,
   }
 }
