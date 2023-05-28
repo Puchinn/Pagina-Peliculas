6cb09189cdd6361eeecb276b1bcfe6f124cda644
Esteban Sayago <sayagoo823@gmail.com>
2023-05-28 14:37:03 -0300

    modificar estilos

--- src/componentes/pelicula/PeliculaDetalles.jsx
+++ src/componentes/pelicula/PeliculaDetalles.jsx
@@ -1,41 +1,37 @@
-import { useParams } from 'react-router-dom'
-import './estilos.css'
-import { useEffect, useState } from 'react'
-import { getMovieDetails } from '../../servicios/getMovieDetails'
-import { useIdiomaContext } from '../../hooks/useIdiomaContext'
+import { useDetalles } from './useDetalles'
 
 export function PeliculaDetalles() {
-  const { peliId } = useParams()
-  const { idioma } = useIdiomaContext()
-  const [detalles, setDetalles] = useState({})
-
-  useEffect(() => {
-    getMovieDetails({ id: peliId, lang: idioma }).then((res) =>
-      setDetalles(res)
-    )
-  }, [peliId, idioma])
+  const { detalles, listaGeneros } = useDetalles()
 
   return (
-    <>
-      <section className='peliculas_detalles'>
-        <div
-          className='fondo'
-          style={{
-            backgroundImage: `url(${detalles.fondoImg})`,
-          }}
-        >
-          <div className='fondoOscuro'></div>
-        </div>
-        <div className='detalles'>
-          <div className='imagen_poster'>
-            <img src={detalles.posterImg} alt={detalles.titulo} />
-          </div>
+    <section className='relative'>
+      <div
+        className=' absolute inset-0 bg-cover min-h-screen'
+        style={{
+          backgroundImage: `url(${detalles.fondoImg})`,
+        }}
+      >
+        <div className='absolute inset-0 bg-black bg-opacity-[93%]'></div>
+      </div>
+      <div className='detalles px-3 gap-x-7 z-10 relative max-w-7xl mx-auto flex py-28'>
+        <img
+          className='w-[350px] rounded-box'
+          src={detalles.posterImg}
+          alt={detalles.titulo}
+        />
+        <div className='space-y-2'>
+          <h1 className='text-5xl font-bold'>{detalles.titulo}</h1>
+          <h2 className='text-xl font-semibold italic'>{detalles.subTitulo}</h2>
+          <h2 className='text-lg font-semibold'>
+            {detalles.lanzamiento} -{' '}
+            <span className='font-bold'>{listaGeneros}</span>
+          </h2>
           <div>
-            <h1>{detalles.titulo}</h1>
-            <p> {detalles.descripcion} </p>
+            <h2 className='text-xl font-semibold pb-3'>Resumen</h2>
+            <p className='font-semibold'> {detalles.descripcion} </p>
           </div>
         </div>
-      </section>
-    </>
+      </div>
+    </section>
   )
 }
