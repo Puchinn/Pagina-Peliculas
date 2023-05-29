a07946ae87068fbdd4d4a895c1ed2cb3fa91ee62
Esteban Sayago <sayagoo823@gmail.com>
2023-05-28 21:25:17 -0300

    abstraer en componente el fondo de imagenes

---
+++ src/componentes/pelicula/FondoDetalles.jsx
@@ -0,0 +1,12 @@
+export function FondoDetalles({ img }) {
+  return (
+    <div
+      className=' absolute inset-0 bg-cover min-h-screen'
+      style={{
+        backgroundImage: `url(${img})`,
+      }}
+    >
+      <div className='absolute inset-0 bg-black bg-opacity-90'></div>
+    </div>
+  )
+}
