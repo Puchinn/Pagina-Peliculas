64aa471786cc7e75fcfbdbae14c7d01d324b9b41
Esteban Sayago <sayagoo823@gmail.com>
2023-05-29 19:46:34 -0300

    actualizar carousel

--- src/paginas/detallesPeliculas/componentes/GaleriaImagenes.jsx
+++ src/paginas/detallesPeliculas/componentes/GaleriaImagenes.jsx
@@ -1,10 +1,24 @@
 import { useParams } from 'react-router-dom'
 import { useEffect, useState } from 'react'
 import { getImages } from '../../../servicios/getImages'
+import AliceCarousel from 'react-alice-carousel'
+import 'react-alice-carousel/lib/alice-carousel.css'
 
 export function GaleriaImagenes() {
   const { peliId: id } = useParams()
   const [imagenes, setImagenes] = useState([])
+
+  const items = imagenes
+    ?.slice(0, 10)
+    .map((img, index) => (
+      <img
+        key={img.iso}
+        className='rounded-2xl py-2 object-cover px-3'
+        width={450}
+        src={img.url}
+        data-value={index}
+      />
+    ))
 
   useEffect(() => {
     getImages({ id: id }).then((res) => setImagenes(res))
@@ -12,25 +26,15 @@ export function GaleriaImagenes() {
 
   return (
     <section className='relative bg-black py-6'>
-      <div className='px-4 py-4 max-w-7xl mx-auto'>
-        {/* <Carousel
-          hideArrow={true}
-          showDots={true}
-          loop={true}
-          autoplay={4000}
-          cols={3}
-          gap={15}
-        >
-          {imagenes?.slice(0, 10).map((img) => (
-            <Carousel.Item key={img.iso}>
-              <img
-                className='rounded-md py-2 object-cover'
-                width={450}
-                src={img.url}
-              />
-            </Carousel.Item>
-          ))}
-        </Carousel> */}
+      <div className='px-4 py-4 max-w-7xl mx-auto shadow-inner'>
+        <AliceCarousel
+          autoWidth={true}
+          autoPlay={true}
+          items={items}
+          infinite={true}
+          autoPlayInterval={3000}
+          disableButtonsControls={true}
+        ></AliceCarousel>
       </div>
     </section>
   )
