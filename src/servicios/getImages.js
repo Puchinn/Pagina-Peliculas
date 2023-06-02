bfc4026a9c90b1a54a95e7cfa31a28ce008ad7c3
Esteban Sayago <sayagoo823@gmail.com>
2023-06-02 18:18:12 -0300

    agregar objeto signal

--- src/servicios/getImages.js
+++ src/servicios/getImages.js
@@ -2,9 +2,9 @@ const { VITE_API_KEY } = import.meta.env
 const api = '?api_key=' + VITE_API_KEY
 import { imagenes } from "../adaptadores/imagenes"
 
-export async function getImages({ id }) {
+export async function getImages({ id }, controlador = {}) {
   const url = `https://api.themoviedb.org/3/movie/${id}/images`
-  const res = await fetch(url + api)
+  const res = await fetch(url + api, { signal: controlador.signal })
   const json = await res.json()
   const formatedImages = imagenes({ arrayImgs: json.backdrops })
   const sortedImages = formatedImages.sort((a, b) => a.puntuacion - b.puntuacion).filter(img => (
