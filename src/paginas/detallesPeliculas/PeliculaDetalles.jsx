980c998095b369c3bcca25d198423ec287bec942
Esteban Sayago <sayagoo823@gmail.com>
2023-06-02 14:41:09 -0300

    actualizar componente

--- src/paginas/detallesPeliculas/PeliculaDetalles.jsx
+++ src/paginas/detallesPeliculas/PeliculaDetalles.jsx
@@ -2,20 +2,28 @@ import { useDetalles } from './useDetalles'
 import { Estrellas } from './componentes/Estrellas'
 import { FondoDetalles } from './componentes/FondoDetalles'
 import { GaleriaImagenes } from './componentes/GaleriaImagenes'
+import { Loader } from './componentes/Loader'
 
 export function PeliculaDetalles() {
-  const { detalles, listaGeneros, hora, fecha } = useDetalles()
+  const { detalles, listaGeneros, hora, fecha, isLoading } = useDetalles()
+
+  if (isLoading) {
+    return <Loader />
+  }
 
   return (
     <>
       <section className='relative '>
         <FondoDetalles img={detalles.fondoImg} />
-        <div className='detalles lg:flex px-3 gap-x-7 z-10 relative max-w-7xl mx-auto py-10  lg:py-28'>
-          <img
-            className='mx-auto my-3 lg:my-0 lg:w-[350px] w-[200px] object-cover rounded-box'
-            src={detalles.posterImg}
-            alt={detalles.titulo}
-          />
+        <div className='lg:flex px-3 gap-x-7 z-10 relative max-w-7xl mx-auto py-10 lg:py-28'>
+          <div className='lg:min-w-[350px] w-[200px] min-h-[300px] lg:min-h-[525px] my-3 lg:my-0 rounded-box bg-base-200 mx-auto lg:mx-0'>
+            <img
+              className='mx-auto h-full w-full object-cover rounded-box '
+              src={detalles.posterImg}
+              alt={detalles.titulo}
+              width={200}
+            />
+          </div>
           <div className='space-y-2'>
             <h1 className='text-4xl sm:text-5xl font-bold'>
               {detalles.titulo}
