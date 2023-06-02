45147559a3223888cdfc7729abb78c5e1ea78a97
Esteban Sayago <sayagoo823@gmail.com>
2023-06-02 14:40:22 -0300

    refactorizar hook

--- src/paginas/detallesPeliculas/useDetalles.js
+++ src/paginas/detallesPeliculas/useDetalles.js
@@ -1,23 +1,20 @@
-import { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 import { useIdiomaContext } from '../../hooks/useIdiomaContext'
 import { getMovieDetails } from '../../servicios/getMovieDetails'
 import { formatedHours } from '../../utils/formatedHours'
+import useSWR from 'swr'
 
 export function useDetalles() {
   const { peliId } = useParams()
   const { idioma } = useIdiomaContext()
-  const [detalles, setDetalles] = useState({})
-  const generos = detalles.generos?.map((obg) => obg.name)
+  const { data, isLoading } = useSWR([peliId, idioma], () => (
+    getMovieDetails({ id: peliId, lang: idioma })
+  ))
+
+  const generos = data && data.generos?.map((obg) => obg.name)
   const listaGeneros = generos?.join("-")
-  const hora = formatedHours({ minutos: detalles.duracion })
-  const fecha = detalles.lanzamiento?.split('-').reverse().join('-')
+  const hora = data && formatedHours({ minutos: data.duracion })
+  const fecha = data && data.lanzamiento?.split('-').reverse().join('-')
 
-  useEffect(() => {
-    getMovieDetails({ id: peliId, lang: idioma }).then((res) =>
-      setDetalles(res)
-    )
-  }, [peliId, idioma])
-
-  return { detalles, listaGeneros, hora, fecha }
+  return { detalles: data, listaGeneros, hora, fecha, isLoading }
 }
