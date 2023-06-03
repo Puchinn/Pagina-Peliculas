732ae83942ea4353d952b9883a138421f729cebc
Esteban Sayago <sayagoo823@gmail.com>
2023-06-02 22:19:33 -0300

    actualizar funcion

--- src/adaptadores/movieDetails.js
+++ src/adaptadores/movieDetails.js
@@ -1,9 +1,11 @@
 export function movideDetails({ obj }) {
+  const isResponsive = screen.width
+  const img = isResponsive <= 800 ? 'w500' : 'original'
   return {
     id: obj.id,
     titulo: obj.title,
     posterImg: 'https://image.tmdb.org/t/p/w500' + obj.poster_path,
-    fondoImg: 'https://image.tmdb.org/t/p/original' + obj.backdrop_path,
+    fondoImg: 'https://image.tmdb.org/t/p/' + img + obj.backdrop_path,
     descripcion: obj.overview,
     generos: obj.genres,
     subTitulo: obj.tagline,
