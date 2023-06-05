dac35a6c9cb4cc8c5fc4bea5aa415ec72c423b1c
Esteban Sayago <sayagoo823@gmail.com>
2023-06-05 12:46:52 -0300

    actualizar estilo

--- src/componentes/loading/Loading.jsx
+++ src/componentes/loading/Loading.jsx
@@ -1,6 +1,6 @@
 export function Loading() {
   return (
-    <div className='w-full flex bg-white bg-opacity-10 justify-center items-center h-screen absolute'>
+    <div className='w-full flex bg-black bg-opacity-70 justify-center items-center h-screen absolute'>
       <span className='loading loading-bars loading-lg'></span>
     </div>
   )
