f375e4c9f8bcc00cb5c94266ac0f6df3fb3ce768
Esteban Sayago <sayagoo823@gmail.com>
2023-06-05 12:46:28 -0300

    verificar que exista un video

--- src/servicios/getVideo.js
+++ src/servicios/getVideo.js
@@ -4,9 +4,15 @@ import { video } from "../adaptadores/video"
 
 export async function getVideo({ id, lang }) {
   const url = `https://api.themoviedb.org/3/movie/${id}/videos`
-  const idioma = '&language' + lang
+  const idioma = '&language=' + lang
   const res = await fetch(url + api + idioma)
   const json = await res.json()
   const keys = video(json)
+  if (keys.length === 0) {
+    const res = await fetch(url + api + '&language=en-US')
+    const json = await res.json()
+    const keys = video(json)
+    return keys
+  }
   return keys
 }
