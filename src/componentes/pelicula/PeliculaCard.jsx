ed01647e0f99144714d2cd776ac7048d4b0a3fac
Esteban Sayago <sayagoo823@gmail.com>
2023-06-11 18:01:29 -0300

    actualizar estilos

--- src/componentes/pelicula/PeliculaCard.jsx
+++ src/componentes/pelicula/PeliculaCard.jsx
@@ -4,13 +4,13 @@ export function PeliculaCard({ objPelicula }) {
   return (
     <div className='max-w-[160px] mx-auto sm:max-w-[200px] text-center px-1'>
       <Link to={`/pelicula/${objPelicula.id}`}>
-        <div className='p-2 bg-base-300 rounded-md space-y-1'>
+        <div className='p-2 rounded-md space-y-1'>
           <img
-            className='rounded-sm w-full object-cover min-h-[185px] xl:min-h-[260px]'
+            className='rounded-md w-full border object-cover min-h-[185px] xl:min-h-[260px]'
             src={objPelicula.posterImg}
             alt={objPelicula.titulo}
           />
-          <p className='text-white font-semibold text-lg break-all whitespace-nowrap overflow-hidden text-ellipsis'>
+          <p className='text-white font-semibold text-lg'>
             {objPelicula.titulo}
           </p>
         </div>
