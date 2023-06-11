384ac8fe62096000dc7a9b38be6cd9216a359468
Esteban Sayago <sayagoo823@gmail.com>
2023-06-11 17:51:04 -0300

    actualizar importacion de pagina Inicio

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import Inicio from './paginas/Inicio'
+import Inicio from './paginas/inicio/Inicio.jsx'
 import Peliculas from './paginas/paginasDePeliculas/peliculasPorCategoria/Peliculas'
 import PeliculaDetalles from './paginas/detallesPeliculas/PeliculaDetalles'
 
