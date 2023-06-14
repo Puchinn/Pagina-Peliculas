a52cacad78a8c2cea8c93c3993479d2e1f034d30
Esteban Sayago <sayagoo823@gmail.com>
2023-06-13 23:27:21 -0300

    refactor: mover carpetas

--- src/paginas/detallesPeliculas/componentes/Loader.jsx
+++
@@ -1,7 +0,0 @@
-export function Loader() {
-  return (
-    <div className='w-full min-h-[80vh] flex justify-center items-center relative z-30 bg-black'>
-      <span className='loading loading-bars loading-lg'></span>
-    </div>
-  )
-}
