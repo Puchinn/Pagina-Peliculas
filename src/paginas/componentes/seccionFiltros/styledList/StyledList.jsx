0015855519efc1f8de803685edc99a20316f83b5
Esteban Sayago <sayagoo823@gmail.com>
2023-06-14 13:24:38 -0300

    actualizar componente

--- src/paginas/componentes/seccionFiltros/styledList/StyledList.jsx
+++ src/paginas/componentes/seccionFiltros/styledList/StyledList.jsx
@@ -1,12 +1,12 @@
 export function StyledList({ children, titulo }) {
   return (
-    <div className='dropdown'>
-      <label tabIndex={0} className='btn btn-outline'>
+    <div className='dropdown dropdown-hover'>
+      <label tabIndex={0} className='btn btn-outline m-1'>
         <h2>{titulo}</h2>
       </label>
       <ul
         tabIndex={0}
-        className='menu menu-sm dropdown-content mt-3 p-2 shadow rounded-box w-52 bg-black/95'
+        className='menu menu-sm dropdown-content p-2 shadow rounded-box w-52 bg-black/95'
       >
         {children}
       </ul>
