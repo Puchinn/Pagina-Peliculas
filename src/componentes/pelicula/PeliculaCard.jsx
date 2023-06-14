79f9a50d39eb835b63f787bfe4323fd32f2384b8
Esteban Sayago <sayagoo823@gmail.com>
2023-06-13 23:15:01 -0300

    actualizar estilos

--- src/componentes/pelicula/PeliculaCard.jsx
+++ src/componentes/pelicula/PeliculaCard.jsx
@@ -1,18 +1,16 @@
 import { Link } from 'react-router-dom'
 
-export function PeliculaCard({ objPelicula }) {
+export function PeliculaCard({ pelicula }) {
   return (
     <div className='max-w-[160px] mx-auto sm:max-w-[200px] text-center px-1'>
-      <Link to={`/pelicula/${objPelicula.id}`}>
+      <Link to={`/pelicula/${pelicula.id}`}>
         <div className='p-2 rounded-md space-y-1'>
           <img
-            className='rounded-md w-full border object-cover min-h-[185px] xl:min-h-[260px]'
-            src={objPelicula.posterImg}
-            alt={objPelicula.titulo}
+            className='rounded-md w-full object-cover min-h-[185px] xl:min-h-[260px]'
+            src={pelicula.posterImg}
+            alt={pelicula.titulo}
           />
-          <p className='text-white font-semibold text-lg'>
-            {objPelicula.titulo}
-          </p>
+          <p className='text-white font-semibold text-lg'>{pelicula.titulo}</p>
         </div>
       </Link>
     </div>
